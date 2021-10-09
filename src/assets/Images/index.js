05dfc9aa35143336c20375d715c9b5a500fcf68b
ismhamza <hamza_dsl@hotmail.com>
2021-10-09 21:21:41 +0500

    explore icons added

--- src/assets/Images/index.js
+++ src/assets/Images/index.js
@@ -1,48 +1,3 @@
-import HomeIcon from './home.png';
-import HomeActiveIcon from './home-active.png';
-import SearchIcon from './search.png';
-import SearchActiveIcon from './search-active.png';
-import BookmarkIcon from './bookmark.png';
-import BookmarkActiveIcon from './bookmark-active.png';
-import MoreIcon from './more.png';
-import MoreActiveIcon from './more-active.png';
-import FileIcon from './file.png';
-import ListIcon from './list.png';
-import GridIcon from './grid.png';
-import TauheedIcon from './tauheed.png';
-import IttebaSunnatIcon from './itteba-e-sunnat.png';
-import TaharatIcon from './taharat.png';
-import NamazIcon from './namaz.png';
-import JanazyIcon from './janazy.png';
-import DaroodIcon from './darood.png';
-import DuaIcon from './dua.png';
-import ZakaatIcon from './zakaat.png';
-import RozonIcon from './rozon.png';
-import UmrahIcon from './umrah.png';
-import JihadIcon from './jihad.png';
-import NikahIcon from './nikah.png';
-import TalaqIcon from './talaq.png';
-import JannatIcon from './jannat.png';
-import JahannamIcon from './jahannam.png';
-import ShafaatIcon from './shafaat.png';
-import QabarIcon from './qabar.png';
-import QayamatIcon from './qayamat.png';
-import DostiIcon from './dosti.png';
-import AlamatEQayamatIcon from './alamat-e-qayamat.png';
-import FazailEQuranIcon from './fazail-e-quran.png';
-import TaleematEQuranIcon from './taleemat-e-quran.png';
-import FazailERehmatUlAlminIcon from './fazail-e-rehmat-ul-almin.png';
-import HaqooqERehmatUlAlminIcon from './haqooq-e-rehmat-ul-almin.png';
-import MasjidIcon from './masjid.png';
-import LibasIcon from './libas.png';
-import AmarBinMaroofIcon from './amar-bin-maroof.png';
-import SagheeraIcon from './sagheera.png';
-import FazailESahabaIcon from './fazail-e-sahaba.png';
-import FazailESahabiyatIcon from './fazail-e-sahabiyat.png';
-import KhanayPeenayIcon from './khanay-peenay.png';
-import TazkeenIcon from './tazkeen.png';
-import TaqdeerIcon from './taqdeer.png';
-// Safar
 import ExploreIcon from './explore.png';
 import ExploreActiveIcon from './explore-active.png';
 import PinIcon from './pin.png';
@@ -51,53 +6,20 @@ import GuideIcon from './guide.png';
 import GuideActiveIcon from './guide-active.png';
 import ReviewsIcon from './reviews.png';
 import ReviewsActiveIcon from './reviews-active.png';
+import LocationIcon from './location.png';
+import FilterIcon from './filter.png';
+import CameraIcon from './camera.png';
+import CommentIcon from './comments.png';
+import LaggageIcon from './laggage.png';
+import LikeIcon from './like.png';
+import StarGrayIcon from './star-gray.png';
+import StarActiveIcon from './star-active.png';
+import Star from './star-unselect.png';
+import VideoIcon from './video.png';
+import User1 from './user1.png';
+import User2 from './user2.png';
 
 export {
-  HomeIcon,
-  HomeActiveIcon,
-  SearchIcon,
-  SearchActiveIcon,
-  BookmarkIcon,
-  BookmarkActiveIcon,
-  MoreIcon,
-  MoreActiveIcon,
-  FileIcon,
-  ListIcon,
-  GridIcon,
-  TauheedIcon,
-  IttebaSunnatIcon,
-  TaharatIcon,
-  NamazIcon,
-  JanazyIcon,
-  DaroodIcon,
-  DuaIcon,
-  ZakaatIcon,
-  RozonIcon,
-  UmrahIcon,
-  JihadIcon,
-  NikahIcon,
-  TalaqIcon,
-  JannatIcon,
-  JahannamIcon,
-  ShafaatIcon,
-  QabarIcon,
-  QayamatIcon,
-  DostiIcon,
-  AlamatEQayamatIcon,
-  FazailEQuranIcon,
-  TaleematEQuranIcon,
-  FazailERehmatUlAlminIcon,
-  HaqooqERehmatUlAlminIcon,
-  MasjidIcon,
-  LibasIcon,
-  AmarBinMaroofIcon,
-  SagheeraIcon,
-  FazailESahabiyatIcon,
-  FazailESahabaIcon,
-  KhanayPeenayIcon,
-  TazkeenIcon,
-  TaqdeerIcon,
-  // Safar
   ExploreIcon,
   ExploreActiveIcon,
   PinIcon,
@@ -106,4 +28,16 @@ export {
   GuideActiveIcon,
   ReviewsIcon,
   ReviewsActiveIcon,
+  LocationIcon,
+  FilterIcon,
+  CameraIcon,
+  CommentIcon,
+  LaggageIcon,
+  LikeIcon,
+  StarGrayIcon,
+  StarActiveIcon,
+  Star,
+  VideoIcon,
+  User1,
+  User2,
 };
--- src/components/IconCustom/IconCustom.js
+++ src/components/IconCustom/IconCustom.js
@@ -1,48 +1,48 @@
-import React, { useState } from "react";
-import { View, Image, StyleSheet, TouchableOpacity } from "react-native";
-import { Loader } from "../Loader";
+import React, {useState} from 'react';
+import {View, Image, StyleSheet, TouchableOpacity} from 'react-native';
+import {Loader} from '../Loader';
 
 const IconCustom = props => {
-    const [isLoadedError, setIsLoadedError] = useState(false);
+  const [isLoadedError, setIsLoadedError] = useState(false);
 
-    const handleOnError = () => {
-        setIsLoadedError(true);
-    };
+  const handleOnError = () => {
+    setIsLoadedError(true);
+  };
 
-    const {
-        width,
-        height,
-        source,
-        isLoading,
-        loaderSize = "small",
-        loaderColor,
-        defaultSize = false,
-        color = "",
-        containerStyles = {},
-        imageStyles = {},
-    } = props;
+  const {
+    width,
+    height,
+    source,
+    isLoading,
+    loaderSize = 'small',
+    loaderColor,
+    defaultSize = false,
+    color = '',
+    containerStyles = {},
+    imageStyles = {},
+  } = props;
 
-    return isLoading ? (
-        <Loader size={loaderSize} color={loaderColor} />
-    ) : (
-        <View style={{ width, height, ...containerStyles }}>
-            <Image
-                
-                onError={handleOnError}
-                source={!isLoadedError ? source : require("../../assets/Images/home.png")}
-                style={[!defaultSize && styles.imageStyles, imageStyles]}
-            />
-        </View>
-    );
+  return isLoading ? (
+    <Loader size={loaderSize} color={loaderColor} />
+  ) : (
+    <View style={{width, height, ...containerStyles}}>
+      <Image
+        onError={handleOnError}
+        source={
+          !isLoadedError ? source : require('../../assets/Images/more.png')
+        }
+        style={[!defaultSize && styles.imageStyles, imageStyles]}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    imageStyles: {
-        resizeMode: "contain",
-        width: "100%",
-        flex: 1
-    }
-})
+  imageStyles: {
+    resizeMode: 'contain',
+    width: '100%',
+    flex: 1,
+  },
+});
 
 export default IconCustom;
-
